3ebd84e3ad68eaa8cfd90627e886cf9e07e0b9ac
Srini <42915005+codewithsrini@users.noreply.github.com>
2018-12-15 19:55:30 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
+import { JobApplicationFormComponent } from './job-application-form/job-application-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    JobApplicationFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/job-application-form/job-application-form.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { JobApplicationFormComponent } from './job-application-form.component';
+
+describe('JobApplicationFormComponent', () => {
+  let component: JobApplicationFormComponent;
+  let fixture: ComponentFixture<JobApplicationFormComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ JobApplicationFormComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(JobApplicationFormComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
